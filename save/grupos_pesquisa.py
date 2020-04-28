0073e627a7ac224c0d849b47f42500c21d6c7f9a
vittorduartte <mateusriograndense@gmail.com>
2020-04-28 16:00:18 -0300

    Inserindo os dados de grupos de pesquisas da UFERSA.

--- save/grupos_pesquisa.py
+++ save/grupos_pesquisa.py
@@ -2,7 +2,7 @@
 from model import  Docente, Curso, Discente, Unidade, Monografia, GrupoPesquisa
 from simpot import serialize_to_rdf_file, mapper_all, serialize_all_to_rdf
 
-from utils import dados_sigaa, dados_ufma, dados_iffar, hashcode
+from utils import dados_sigaa, dados_ufma, dados_iffar, dados_csv, hashcode
 
 
 
@@ -34,11 +34,25 @@ serialize_rdf_grupopesquisa = {
                     "id": lambda d : hashcode ("iffar", str(d["id_grupo_pesquisa"])),
                     "area" : lambda d: d["links"]["id_area_conhecimento_cnpq"]["title"].split(':')[1].strip(),
                     "university" : lambda d: "http://dbpedia.org/resource/Federal_Institute_Farroupilha",
-                    "coordenador": lambda d: "https://www.dbacademic.tech/resource/" +  hashcode ( "iffar", str (d["links"]["id_coordenador"]["title"].split(":")[1].strip()))
+                    "coordenador": lambda d: "https://www.dbacademic.tech/resource/" +  hashcode ( "iffar", str (d["links"]["id_coordenador"]["title"].split(":")[0].strip()))
             },
 
             "data" : lambda :  dados_iffar("http://dados.iffarroupilha.edu.br/api/v1/grupos-pesquisa.json"),
             "rdf_path" : "rdf/grupospesquisa_iffar.rdf"
+        },
+
+        { ## ufersa
+            "toSave" : True,
+            "mapper" : {
+                    "nome" : "titulo_projeto", 
+                    "id": lambda d : hashcode ("ufersa", str(d["codigo"])),
+                    "area" : lambda d: "area",
+                    "coordenador": lambda d: "https://www.dbacademic.tech/resource/" +  hashcode("ufersa", str(d["codigo"])),
+                    "university" : lambda d: "http://dbpedia.org/page/Federal_Rural_University_of_the_Semi-arid_Region",
+            },
+
+            "data" : lambda :  dados_csv("http://api.ufersa.edu.br:8080/apiufersa/rest/pda/projetos-pesquisa"),
+            "rdf_path" : "rdf/grupospesquisa_ufersa.rdf"
         },
 
 
